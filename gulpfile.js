3b71cf248f6e2d87be5305d30e749db247c0cf7d
adgai19 <gaitondeaditya@gmail.com>
2019-09-25 21:26:23 +0530

    minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 const { src, dest, parallel } = require('gulp');
 const htmlmin = require('gulp-htmlmin');
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-const jsonminify=require('gulp-jsonminify');
+//const jsonminify=require('gulp-jsonminify');
+const minifycss=require('gulp-clean-css');
 function processHTML(){
         return src('src/*.html').pipe(htmlmin({collapseWhitespace:true})).pipe(dest('dist'));
 }
 
-// /*function imageminify(){
-//         return src('src/img/**/*') .pipe(imagemin()) .pipe(dest('dist/img/'));
-// }*/
+function imageminify(){
+         return src('src/img/**/*') /*.pipe(imagemin()) */.pipe(dest('dist/img/'));
+ }
 
 function jsminify(){
-        return src('src/*js').pipe(uglify()).pipe(dest('dist/'));
+        return src('src/scripts/*js').pipe(uglify()).pipe(dest('dist/scripts/'));
+}
+function cssminify(){
+        return src('src/css/*.css').pipe(minifycss({compatability:'ie'})).pipe(dest(('dist/css/')))
 }
 //let jsonprocess=()=> src('src/*.json').pipe(jsonminify()).pipe(dest('dist'));
 
